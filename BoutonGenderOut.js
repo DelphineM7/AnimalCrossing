91b01b822afe66f51abeb477fbc612a738f54411
DelphineM7 <95476951+DelphineM7@users.noreply.github.com>
2024-02-02 18:42:02 +0100

    adaptataion aux différents écrans

--- BoutonGenderOut.js
+++ BoutonGenderOut.js
@@ -36,15 +36,15 @@ function BoutonGenderOut(){
             .duration(1000) 
             .attr('height', 50 )
             .attr('width', 50)
-            .attr('x', milieuX+90 )
-            .attr('y', milieuY+190 ) 
+            .attr('x', ((milieuX+85) *addaptation) + addaptationX)
+            .attr('y', ((milieuY+185) *addaptation) + addaptationY) 
                 
         etoile_bleu.transition()
             .duration(1000) 
             .attr('height', 50 )
             .attr('width', 50 )
-            .attr('x', milieuX+60 )
-            .attr('y', milieuY-190 ) 
+            .attr('x', ((milieuX+60) *addaptation) + addaptationX)
+            .attr('y', ((milieuY-190) *addaptation) + addaptationY)         
         
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
@@ -60,15 +60,15 @@ function BoutonGenderOut(){
             .duration(1000) 
             .attr('height', 50 )
             .attr('width', 50 )
-            .attr('x', milieuX+90 -90)
-            .attr('y', milieuY+190)             
+            .attr('x', ((milieuX+90-90)*addaptation) + addaptationX +50)
+            .attr('y', ((milieuY+190)*addaptation) + addaptationY)            
 
         etoile_bleu.transition()
             .duration(1000) 
             .attr('height', 50)
             .attr('width', 50)
-            .attr('x', milieuX-40-90)
-            .attr('y', milieuY-190) 
+            .attr('x', ((milieuX-40-90)*addaptation) + addaptationX -5+50)
+            .attr('y', ((milieuY-190)*addaptation) + addaptationY-5) 
 
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
@@ -83,15 +83,15 @@ function BoutonGenderOut(){
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX+180)
-        .attr('y', milieuY+40) 
+        .attr('x', ((milieuX+180)*addaptation) + addaptationX)
+        .attr('y', ((milieuY+40)*addaptation) + addaptationY)
 
         etoile_bleu.transition()
         .duration(1000) 
         .attr('height', 50)
         .attr('width', 50)
-        .attr('x', milieuX+150)
-        .attr('y', milieuY-150) 
+        .attr('x', ((milieuX+150)*addaptation) + addaptationX)
+        .attr('y', ((milieuY-150)*addaptation) + addaptationY)
 
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
@@ -105,15 +105,15 @@ function BoutonGenderOut(){
         .duration(1000) 
         .attr('height', 50)
         .attr('width', 50)
-        .attr('x', milieuX + 110-70)
-        .attr('y', milieuY + 230)
+        .attr('x', ((milieuX + 110-70)*addaptation) + addaptationX) 
+        .attr('y', ((milieuY + 230)*addaptation) + addaptationY) 
 
         etoile_bleu.transition()
         .duration(1000) 
         .attr('height', 50)
         .attr('width', 50)
-        .attr('x', milieuX + 290-70)
-        .attr('y', milieuY - 140)
+        .attr('x', ((milieuX + 290-70)*addaptation) + addaptationX)
+        .attr('y', ((milieuY - 140)*addaptation) + addaptationY)
 
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
@@ -127,15 +127,15 @@ function BoutonGenderOut(){
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 130-150)
-        .attr('y', milieuY - 220)
+        .attr('x', ((milieuX + 130-150)*addaptation) + addaptationX)
+        .attr('y', ((milieuY - 220)*addaptation) + addaptationY)
 
         etoile_bleu.transition()
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 300-150)
-        .attr('y', milieuY - 250)
+        .attr('x', ((milieuX + 300-150)*addaptation) + addaptationX)
+        .attr('y', ((milieuY - 250)*addaptation) + addaptationY)
 
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
@@ -149,15 +149,15 @@ function BoutonGenderOut(){
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 65)
-        .attr('y', milieuY + 85)
+        .attr('x', ((milieuX + 65)*addaptation) + addaptationX)
+        .attr('y', ((milieuY + 85)*addaptation) + addaptationY)
 
         etoile_bleu.transition()
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX - 290)
-        .attr('y', milieuY - 300)
+        .attr('x', ((milieuX - 290)*addaptation) + addaptationX)
+        .attr('y', ((milieuY - 300)*addaptation) + addaptationY)
 
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
@@ -171,15 +171,15 @@ function BoutonGenderOut(){
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 120)
-        .attr('y', milieuY + 65)
+        .attr('x', ((milieuX + 120)*addaptation) + addaptationX)
+        .attr('y', ((milieuY + 65)*addaptation) + addaptationY)
 
         etoile_bleu.transition()
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 100)
-        .attr('y', milieuY - 340) 
+        .attr('x', ((milieuX + 100)*addaptation) + addaptationX)
+        .attr('y', ((milieuY - 340)*addaptation) + addaptationY) 
         
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
@@ -193,15 +193,15 @@ function BoutonGenderOut(){
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 145)
-        .attr('y', milieuY + 65)
+        .attr('x', ((milieuX + 145)*addaptation) + addaptationX)
+        .attr('y', ((milieuY + 65)*addaptation) + addaptationY)
 
         etoile_bleu.transition()
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX - 60)
-        .attr('y', milieuY - 380)
+        .attr('x', ((milieuX - 60)*addaptation) + addaptationX)
+        .attr('y', ((milieuY - 380)*addaptation) + addaptationY)
 
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
@@ -215,15 +215,15 @@ function BoutonGenderOut(){
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 228)
-        .attr('y', milieuY + 259)
+        .attr('x', ((milieuX + 228)*addaptation) + addaptationX)
+        .attr('y', ((milieuY + 259)*addaptation) + addaptationY)
 
         etoile_bleu.transition()
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX - 295)
-        .attr('y', milieuY - 345)
+        .attr('x', ((milieuX - 295)*addaptation) + addaptationX)
+        .attr('y', ((milieuY - 345)*addaptation) + addaptationY)
 
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
@@ -237,15 +237,15 @@ function BoutonGenderOut(){
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 42)
-        .attr('y', milieuY + 20)
+        .attr('x', ((milieuX + 42)*addaptation) + addaptationX -5)
+        .attr('y', ((milieuY + 20)*addaptation) + addaptationY -5)
 
         etoile_bleu.transition()
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 115)
-        .attr('y', milieuY - 270)
+        .attr('x', ((milieuX + 115)*addaptation) + addaptationX)
+        .attr('y', ((milieuY - 270)*addaptation) + addaptationY) 
         
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
@@ -259,15 +259,15 @@ function BoutonGenderOut(){
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 32)
-        .attr('y', milieuY - 85)
+        .attr('x', ((milieuX + 32)*addaptation) + addaptationX)
+        .attr('y', ((milieuY - 85)*addaptation) + addaptationY)
 
         etoile_bleu.transition()
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 166)
-        .attr('y', milieuY + 40) 
+        .attr('x', ((milieuX + 166)*addaptation) + addaptationX)
+        .attr('y', ((milieuY + 40)*addaptation) + addaptationY) 
 
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
@@ -281,15 +281,15 @@ function BoutonGenderOut(){
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 32-200)
-        .attr('y', milieuY - 105)
+        .attr('x', ((milieuX + 32 -200) *addaptation) + addaptationX)
+        .attr('y', ((milieuY - 105)*addaptation) + addaptationY)
 
         etoile_bleu.transition()
         .duration(1000)
         .attr('height', 50)
         .attr('width', 50) 
-        .attr('x', milieuX + 106-200)
-        .attr('y', milieuY + 60) 
+        .attr('x', ((milieuX + 106 -200)*addaptation) + addaptationX)
+        .attr('y', ((milieuY + 60)*addaptation) + addaptationY)
 
         // variable pour Diagramme en barre selon :  SIGNE - SEXE - ESPECE
             //Male
